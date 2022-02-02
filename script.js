8134090732910cd7883666df32c95a835ac394b2
JHokum <jdelgado92@hotmail.com>
2022-02-01 18:42:23 -0800

    Functional grid 16x16 grid is functional with some test styling

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 //create a 16x16 grid of squares in the html
-const row = 16;
+const rows = 16;
 const columns= 16;
+let divContainer;
+let divColumn;
 
-
-
+//div container
+divContainer = document.createElement("div");
+divContainer.classList.add("container");
 //body selection
 const body = document.querySelector("body");
-//div to hold columns
-const divContainer = document.createElement("div");
-divContainer.classList.add("container");
 
 
 
 //16 div containers, column display
-for (let i = 0; i<16; i++){
-    divColumn = document.createElement("div")
-    divColumn.classList.add("column");
-    divColumn.textContent="meow";
-    divContainer.appendChild(divColumn);
+function columnsOfNum(num){
+    
+    for (let i = 0; i<num; i++){
+        divColumn = document.createElement("div")
+        divColumn.classList.add("column");
+        rowsOfNum(divColumn,rows);
+        // divColumn.textContent="meow";
+        divContainer.appendChild(divColumn);
+    }
+    body.append(divContainer);
 }
+//16 rows
+function rowsOfNum(column,num){
+    console.log(column);
+    for(let i =0; i<num; i++){
+        divRow = document.createElement("div");
+        divRow.classList.add("row");
+        column.appendChild(divRow);
+        
+    }
+}
+//creates grid
+columnsOfNum(columns);
 
-//append divContainer
-body.append(divContainer);
+
 //16 divs with square class, square styled in css
